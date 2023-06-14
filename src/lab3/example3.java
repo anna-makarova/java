aacdeb9dd27972571323fc60e4b964b0b9e416ef
Ann <a.s.makarovaa@gmail.com>
2023-06-14 18:31:17 +0500

    lab 3

---
+++ src/lab3/example3.java
@@ -0,0 +1,26 @@
+package lab3;
+
+public class example3 {
+    public static int step = 0;
+
+    public static void main(int x) {
+        space();
+        System.out.println("" + x + "->");
+        step++;
+        if ((2*x+1) < 20) {
+            main(2 * x + 1);
+        }
+        step--;
+        space();
+        System.out.println("" + x + "<-");
+    };
+
+    public static void space() {
+        for (int i = 0; i < step; i++) {
+            System.out.print(" ");
+        }
+    }
+    public static void main(String[] args) {
+        main(1);
+    }
+}
